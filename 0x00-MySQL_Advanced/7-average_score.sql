e66fc5e95d75fc9e1a18605ef4a09929c33bd04b
Mitchkal <mitchellkalenda@gmail.com>
2024-03-21 13:24:44 -0400

    added stored procedure for avergae score

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,17 @@
+-- stored procedure to calculate average score of users
+
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser (
+    IN user_id INT
+)
+BEGIN
+    DECLARE avg_score DECIMAL(10,2);
+
+    SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
+
+    UPDATE users SET average_score = avg_score WHERE id = user_id;
+
+END//
+
+DELIMITER ;
