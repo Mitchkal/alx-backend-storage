753cc96e73767ba59aeecbdf387343302cee8ea0
Mitchkal <mitchellkalenda@gmail.com>
2024-03-21 14:53:36 -0400

    added 11-need_meeting

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,47 +1,9 @@
 -- create meeting as needed
-
+DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
-SELECT 
-    users.name
-FROM 
-    users
-LEFT JOIN 
-    (
-        SELECT 
-            user_id, 
-            MAX(meeting_date) AS last_meeting
-        FROM 
-            meetings
-        GROUP BY 
-            user_id
-    ) AS last_meeting_dates
-ON 
-    users.id = last_meeting_dates.user_id
-LEFT JOIN 
-    (
-        SELECT 
-            user_id, 
-            MAX(score_date) AS last_score
-        FROM 
-            scores
-        GROUP BY 
-            user_id
-    ) AS last_score_dates
-ON 
-    users.id = last_score_dates.user_id
-LEFT JOIN 
-    (
-        SELECT 
-            user_id, 
-            AVG(score) AS avg_score
-        FROM 
-            scores
-        GROUP BY 
-            user_id
-    ) AS avg_scores
-ON 
-    users.id = avg_scores.user_id
-WHERE 
-    (avg_scores.avg_score < 80 OR avg_scores.avg_score IS NULL)
-    AND (last_meeting_dates.last_meeting IS NULL OR last_meeting_dates.last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH));
-
+SELECT name FROM students
+WHERE
+    score < 80 AND
+    (last_meeting is NULL
+        OR
+    last_meeting < ADDDATE(CURDATE(), interval - 1 MONTH));
