ce1e565c264125d96a03422a1b3ac1a878992713
Mitchkal <mitchellkalenda@gmail.com>
2024-03-21 08:44:59 -0400

    refactored count by fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- ranks country origins of bands ordered b number of non unique fans
 
-SELECT origin AS origin, COUNT(*) as nb_fans
+SELECT origin AS origin, COUNT(fans) as nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
