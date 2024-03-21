03d2d44d23f2f70943bbe429f70072b5ed850fa5
Mitchkal <mitchellkalenda@gmail.com>
2024-03-21 11:08:35 -0400

    added trigger to decrease quantity of item after adding new order

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,14 @@
+-- trigger todecrease quantity of item after adding new order
+DELIMITER //
+
+
+CREATE TRIGGER decrease_quantity_after_order
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END//
+
+DELIMITER ;
