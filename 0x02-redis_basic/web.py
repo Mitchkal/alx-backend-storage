15d99ce942ceacc2202dc8ca9883447901f34cf6
Mitchkal <mitchellkalenda@gmail.com>
2024-03-27 19:29:42 -0400

    added track and cache page|

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+module for get page function
+"""
+
+import redis
+from requests import get
+from functools import wraps
+from typing import Callable
+
+
+cache = redis.Redis()
+
+
+def track_url(method: Callable) -> Callable:
+    """
+    count decorator
+    """
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        url = args
+        key = f"count:{url}"
+
+        cache.incr(key)
+        cache_content = cache.get(key)
+
+        if cache_content:
+            return cache_content.decode('utf-8')
+
+        html = method(url)
+        cache.setex(key, 10, html)
+        return html
+    return wrapper
+
+
+@track_url
+def get_page(url: str) -> str:
+    data = get(url)
+    return data.text
