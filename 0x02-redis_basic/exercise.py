00ff7df7abc21bd11fc5fdedc9aca4523f8357f2
Mitchkal <mitchellkalenda@gmail.com>
2024-03-27 18:55:58 -0400

    added replay function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -48,6 +48,23 @@ def call_history(method: Callable) -> Callable:
     return wrapper
 
 
+def replay(method: Callable) -> None:
+    """
+    displays history of calls for given function
+    """
+    name = method.__qualname__
+
+    cache = redis.Redis()
+    calls = cache.get(name).decode('utf-8')
+    print(f"{name} was called {calls} times:")
+
+    inputs = cache.lrange(name + ":inputs", 0, -1)
+    outputs = cache.lrange(name + ":outputs", 0, -1)
+
+    for i, o in zip(inputs, outputs):
+        print(f"{name}(*{i.decode('utf-8')}) -> {o.decode('utf-8')}")
+
+
 class Cache:
     """
     class definition for cache
